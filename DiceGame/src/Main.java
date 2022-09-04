1881a9ae4f1c483814ccd1966743ccb351066144
oxsalis <SarahOxley@myop.ac.nz>
2022-09-05 11:57:55 +1200

    Initial upload to GitHub of the Dice Game

---
+++ DiceGame/src/Main.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+    public static void main(String args[])
+    {
+        System.out.print("Enter the number of dice your rolling: ");
+
+        Scanner input = new Scanner(System.in);
+        int numberOfDice = input.nextInt();
+
+        Random ranNum = new Random();
+
+        System.out.print("Hey Player! You rolled: ");
+        int total = 0;
+        int randomNumber = 0;
+
+        for (int i = 0; i < numberOfDice; i++) {
+
+            randomNumber = ranNum.nextInt(6) + 1;
+            total = total + randomNumber;
+            System.out.print(randomNumber);
+            System.out.print(" ");
+        }
+
+        System.out.println("");
+        System.out.println("Total amount on dice: " + total);
+        input.close();
+    }
+}
