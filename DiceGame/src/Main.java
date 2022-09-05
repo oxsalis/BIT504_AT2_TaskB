cd0de01ac0a283502dd71715f4be6e546d42518e
oxsalis <SarahOxley@myop.ac.nz>
2022-09-05 12:06:12 +1200

    added comments to the code

--- DiceGame/src/Main.java
+++ DiceGame/src/Main.java
@@ -1,18 +1,20 @@
 
-import java.util.Scanner;
-import java.util.Random;
+import java.util.Scanner; //scans for input
 
-public class Main {
+import java.util.Random; //randomizes the number selected
+
+//Dice Game allows uses to enter how many dice and the code displays a random number between 1-6 with total
+public class Main { 
     public static void main(String args[])
     {
-        System.out.print("Enter the number of dice your rolling: ");
+        System.out.print("Enter the number of dice your rolling: "); //asks input for how many dice to use
 
         Scanner input = new Scanner(System.in);
         int numberOfDice = input.nextInt();
 
-        Random ranNum = new Random();
+        Random ranNum = new Random(); //populates a random number
 
-        System.out.print("Hey Player! You rolled: ");
+        System.out.print("Hey Player! You rolled: "); //displays the random number between 1-6
         int total = 0;
         int randomNumber = 0;
 
@@ -25,7 +27,8 @@ public class Main {
         }
 
         System.out.println("");
-        System.out.println("Total amount on dice: " + total);
+        System.out.println("Total amount on dice: " + total); //adds the total number of randomizes number from dice
         input.close();
-    }
+        
+    }//end of program
 }
