6f5706e93e84cab20b88117d3dab41976b439976
Hyeonseong Lee <82516345+Polobear9@users.noreply.github.com>
2022-09-05 12:06:19 +0900

    Update Main.java
    Change ver 2

--- DiceGame/src/Main.java
+++ DiceGame/src/Main.java
@@ -1,27 +1,24 @@
+import java.util.Scanner;
+import java.util.Random;
 
-import java.util.Scanner; //scans for input
-
-import java.util.Random; //randomizes the number selected
-
-//Dice Game allows uses to enter how many dice and the code displays a random number between 1-6 with total
-public class Main { 
-    public static void main(String args[])
-    {
-        System.out.print("Enter the number of dice your rolling: "); //asks input for how many dice to use
-
-        Scanner input = new Scanner(System.in);
-        int numberOfDice = input.nextInt();
-
-        Random ranNum = new Random(); //populates a random number
-
-        System.out.print("Hey Player! You rolled: "); //displays the random number between 1-6
+public class sample {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in); // or (it can be sc)
+        Random random = new Random(); // also it can be (ran)
+        //populates a random number
         int total = 0;
         int randomNumber = 0;
 
+        System.out.print("Enter the number of dice your rolling: "); //asks input for how many dice to use
+        int numberOfDice = scanner.nextInt();
+
+
+        System.out.print("Hey Player! You rolled: "); //displays the random number between 1-6
+
         for (int i = 0; i < numberOfDice; i++) {
 
-            randomNumber = ranNum.nextInt(6) + 1;
-            total = total + randomNumber;
+            randomNumber = random.nextInt(6) + 1;
+            total += randomNumber;
             System.out.print(randomNumber);
             System.out.print(" ");
         }
@@ -29,7 +26,7 @@ public class Main {
         System.out.println("");
         System.out.println("Total amount on dice: " + total); //adds the total number of randomizes number from dice
         System.out.println("Thanks for playing"); //ends game
-        input.close();
-        
+        scanner.close();
+
     }//end of program
 }
